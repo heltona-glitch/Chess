f3f4e80843384ac88d237a594c3a3c22532200d1
heltona-glitch <heltona@student.ncmich.edu>
2026-05-08 01:57:10 -0400

    Added GEH because I almost forgot. now final!

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseMiddleware<ExceptionMiddleware>();
     app.UseDeveloperExceptionPage();
     app.MapOpenApi();
     app.UseSwagger();
